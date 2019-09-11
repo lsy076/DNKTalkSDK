8f2e13fb456d4d786c40ac8aac7feaae969a1276
赖盛源 <756486059@qq.com>
2019-09-11 20:01:42 +0800

    上传第一个版本

---
+++ DNKTalkSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DNKTalkSDK
+//
+//  Created by 赖盛源 on 2019/9/11.
+//  Copyright © 2019 dnake_ay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DNKTalkSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DNKTalkSDK
+//
+//  Created by 赖盛源 on 2019/9/11.
+//  Copyright © 2019 dnake_ay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
