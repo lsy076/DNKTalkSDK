52ece76af7e903e1bec5f766bd38b92518ed5a54
赖盛源 <756486059@qq.com>
2019-12-23 14:53:48 +0800

    no message

--- DNKTalkSDK/ViewController.m
+++ DNKTalkSDK/ViewController.m
@@ -120,9 +120,7 @@
     
     [MBProgressHUD showHUDAddedTo:self.view animated:YES];
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        NSLog(@"%@", sipConfigEntity);
-    });
+    NSLog(@"%@", sipConfigEntity);
     
 }
 //监视
